00f8e879cd3b7ab6c10807c368631bcbcfa606ed
aothymoon59 <mmoon8745@gmail.com>
2023-09-08 01:25:00 +0600

    static sidebar done

--- src/Layout/Sidebar.jsx
+++ src/Layout/Sidebar.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import { Link, NavLink } from 'react-router-dom'
-import { FaHome } from 'react-icons/fa'
+import { FaHome, FaUserCircle } from 'react-icons/fa'
+import { MdLibraryAdd } from 'react-icons/md'
+import { GoTasklist } from 'react-icons/go'
 import { GrLogout } from 'react-icons/gr'
-import { FcSettings } from 'react-icons/fc'
 import { AiOutlineBars } from 'react-icons/ai'
 import Logo from '../Components/Logo/Logo'
 
@@ -58,6 +59,24 @@ const Sidebar = () => {
                                 <FaHome className='w-5 h-5' />
                                 <span className="mx-4 font-medium">Home</span>
                             </NavLink>
+                            <NavLink
+                                to="/add-task"
+                                className={({ isActive }) =>
+                                    `flex items-center px-4 py-2 mt-5  transition-colors duration-300 transform  hover:bg-blue-300 text-black ${isActive ? "bg-blue-300" : "bg-transparent"}`
+                                }
+                            >
+                                <MdLibraryAdd className='w-5 h-5' />
+                                <span className="mx-4 font-medium">Add Task</span>
+                            </NavLink>
+                            <NavLink
+                                to="/task-board"
+                                className={({ isActive }) =>
+                                    `flex items-center px-4 py-2 mt-5  transition-colors duration-300 transform  hover:bg-blue-300 text-black ${isActive ? "bg-blue-300" : "bg-transparent"}`
+                                }
+                            >
+                                <GoTasklist className='w-5 h-5' />
+                                <span className="mx-4 font-medium">Task Board</span>
+                            </NavLink>
                         </nav>
                     </div>
                 </div>
@@ -70,7 +89,7 @@ const Sidebar = () => {
                             `flex items-center px-4 py-2 mt-5  transition-colors duration-300 transform  hover:bg-blue-300 text-black ${isActive ? "bg-blue-300" : "bg-transparent"}`
                         }
                     >
-                        <FcSettings className='w-5 h-5' />
+                        <FaUserCircle className='w-5 h-5' />
 
                         <span className='mx-4 font-medium'>Profile</span>
                     </NavLink>
